734d081b037931d7bd7ea555f6351eba0922627a
Frederick Wells <pandabrand@gmail.com>
2022-05-04 11:36:01 -0500

    inbox screen

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,17 +1,28 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-
-const defaultTasks = [
-  { id: '1', title: 'Something', state: 'TASK_INBOX' },
-  { id: '2', title: 'Something more', state: 'TASK_INBOX' },
-  { id: '3', title: 'Something else', state: 'TASK_INBOX' },
-  { id: '4', title: 'Something again', state: 'TASK_INBOX' },
-];
+import {
+  configureStore,
+  createSlice,
+  createAsyncThunk,
+} from '@reduxjs/toolkit';
+import { async } from 'q';
 
 const TaskBoxData = {
-  tasks: defaultTasks,
-  statud: 'idle',
+  tasks: [],
+  status: 'idle',
   error: null,
 };
+
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/todos?userId=1'
+  );
+  const data = await response.json();
+  const result = data.map((task) => ({
+    id: `${task.id}`,
+    title: task.title,
+    state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+  }));
+  return result;
+});
 
 const TasksSlice = createSlice({
   name: 'taskbox',
@@ -23,7 +34,25 @@ const TasksSlice = createSlice({
       if (task >= 0) {
         state.tasks[task].state = newTaskState;
       }
-    }
+    },
+  },
+  extraReducers(builder) {
+    builder
+    .addCase(fetchTasks.pending, (state) => {
+      state.status = 'loading';
+      state.error = null;
+      state.tasks = [];
+    })
+    .addCase(fetchTasks.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.error = null;
+      state.tasks = action.payload;
+    })
+    .addCase(fetchTasks.rejected, (state) => {
+      state.status = 'failed';
+      state.error = "Something went wrong";
+      state.tasks = [];
+    });
   }
 });
 
